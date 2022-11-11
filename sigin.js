8dc250f5f475ce814ad638487a46ababbc1605f4
malonicester123 <ashish.tripathy38@gmail.com>
2022-11-11 17:35:12 +0530

    sign in page done

---
+++ sigin.js
@@ -0,0 +1,26 @@
+console.log('hello world');
+let siginData = JSON.parse(localStorage.getItem('signin')) || [];
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let obj = {
+        name:form.email.value,
+        email:form.first_Name.value,
+        lastName:form.last_Name.value,
+        password:form.password.value,
+    }
+    let found = false;
+    siginData.forEach((item) => {
+        if(item.email == obj.email){
+            found = true;
+            alert("You are already registered");
+        }
+    });
+    if(!found){
+       siginData.push(obj);
+       localStorage.setItem('signin',JSON.stringify(siginData));
+       alert('Registration Successful');
+    }
+})
+console.log(document.querySelector('#log_in'))
+
